e1f9ee5db2acb41719119d5b798308fd9f54505e
Abdulla-hamideh <Abdullahhamideh@outlook.com>
2021-04-12 08:36:24 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -16,9 +16,12 @@ def scrape_table(url):
     return df
 
 # macrotrend analysis
-stock_list = ["AAPL/apple","MSFT/microsoft",'AMZN/amazon',"TSLA/tesla","NVDA/nvidia","FB/facebook","GOOGL/alphabe","JPM/jpmorgan-chase","C/citigroup","BCS/barclays","NFLX/netflix","BA/boeing"]
+stock_list = ["AAPL/apple","MSFT/microsoft",'AMZN/amazon',"TSLA/tesla","NVDA/nvidia","FB/facebook","GOOGL/alphabe","JPM/jpmorgan-chase","NFLX/netflix","BA/boeing","NKE/nike","WMT/walmart","V/visa","PYPL/paypal-holdings","BAC/bank-of-america"]
 # ychart analysis
-stocks = ["AAPL", "MSFT", "AMZN", "TSLA", "NVDA", "FB", "GOOG", "JPM", "C", "BCS", "NFLX","BA","PFE"]
+stocks = ["AAPL", "MSFT", "AMZN", "TSLA", "NVDA", "FB", "GOOG", "JPM", "NFLX","BA","NKE","WMT","V","PYPL","BAC"]
+
+# companies names
+names = ["Apple","Microsoft","Amazon","Tesla","NVIDIA","Facebook","Alphabet","JPMorgan Chase","Netflix","Boeing","NIKE","Walmart","Visa","PayPal Holdings","Bank Of America"]
 
 # company is for decideing which website to take
 # company 2 just the full for the company to plot the diagrams
@@ -36,9 +39,9 @@ def get_url(company,company2):
                     print(hist)
                     # print the name of the columns
                     #print(hist.columns)
-                    x =hist[company2+ ' PE Ratio Historical Data',        'Date']
-                    y = hist[company2+' PE Ratio Historical Data',    'PE Ratio']
-                    z= hist[company2+' PE Ratio Historical Data', 'Stock Price']
+                    x =hist[company2 + ' PE Ratio Historical Data','Date']
+                    y = hist[company2 +' PE Ratio Historical Data', 'PE Ratio']
+                    z= hist[company2 +' PE Ratio Historical Data', 'Stock Price']
                     plt.plot(x,y, label = "P/E ratio")
                     plt.plot(x,z, label = "stock price")
                     plt.legend()
@@ -50,8 +53,8 @@ def get_url(company,company2):
                     hist = scrape_table(url_completo)
                     hist = hist.sort_index(ascending=False)
                     print(hist)
-                    # print the name of the columns
-                    # print(hist.columns)
+                    #print the name of the columns
+                    #print(hist.columns)
                     x = hist[company2+ " Annual EPS"]
                     y = hist[company2 +" Annual EPS.1"]
                     plt.bar(x, y, label = "Eps")
@@ -64,8 +67,8 @@ def get_url(company,company2):
                     hist = scrape_table(url_completo)
                     hist = hist.sort_index(ascending=False)
                     hist.dropna()
-                    # print the name of the columns
-                    # print(hist.columns)
+                    #print the name of the columns
+                    #print(hist.columns)
                     x = hist[company2+' Price/Book Ratio Historical Data',                 'Date']
                     y = hist[company2+' Price/Book Ratio Historical Data',  'Price to Book Ratio']
                     z = hist[company2+' Price/Book Ratio Historical Data',          'Stock Price']
@@ -87,6 +90,8 @@ def get_url(company,company2):
                     plt.title(s)
                     plt.show()
                     plt.bar(x, zz, label="BVPS")
+                    plt.title(s)
+                    plt.legend()
                     plt.show()
         else:
             for i in stocks:
@@ -101,8 +106,31 @@ def get_url(company,company2):
                     print(hist)
                     #plotting the results
                     plt.bar(hist["Data for this Date Range"],hist["Unnamed: 1"])
+                    plt.title("Profit margin "+i)
                     plt.show()
+            break
 
-print(get_url("TSLA",None))
+
+#merging the two list to get the info for the all companies
+stock_list_temp = stock_list
+
+merge = stock_list_temp+ stocks
+
+#printing all results 
+for i in merge:
+    if i in stock_list_temp:
+        for n in stock_list_temp:
+            for k in names:
+                print(get_url(n,k))
+                names.remove(k)
+                stock_list_temp.remove(n)
+                break
+            break
+    else:
+        print(get_url(i,None))
+
+
+
+
 
 
