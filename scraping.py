08fcbdbe13d27f0eacaf420cebc61e2aa7d2a7e7
Abdulla-hamideh <Abdullahhamideh@outlook.com>
2021-04-12 09:31:56 +0100

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -26,106 +26,103 @@ names = ["Apple","Microsoft","Amazon","Tesla","NVIDIA","Facebook","Alphabet","JP
 # company is for decideing which website to take
 # company 2 just the full for the company to plot the diagrams
 def get_url(company,company2):
-    for s in stock_list:
-        if s == company:
-            # macro
-            url = ["https://www.macrotrends.net/stocks/charts/" + s + "/pe-ratio","https://www.macrotrends.net/stocks/charts/"+ s +"/eps-earnings-per-share-diluted","https://www.macrotrends.net/stocks/charts/"+s+"/price-book" ]
-            for ll in url:
-                if ll == "https://www.macrotrends.net/stocks/charts/" + s + "/pe-ratio":
-                    data = requests.request("GET",ll)
-                    url_completo = data.url
-                    hist = scrape_table(url_completo)
-                    hist = hist.sort_index(ascending=False)
-                    print(hist)
-                    # print the name of the columns
-                    #print(hist.columns)
-                    x =hist[company2 + ' PE Ratio Historical Data','Date']
-                    y = hist[company2 +' PE Ratio Historical Data', 'PE Ratio']
-                    z= hist[company2 +' PE Ratio Historical Data', 'Stock Price']
-                    plt.plot(x,y, label = "P/E ratio")
-                    plt.plot(x,z, label = "stock price")
-                    plt.legend()
-                    plt.title(s)
-                    plt.show()
-                elif ll == "https://www.macrotrends.net/stocks/charts/"+ s +"/eps-earnings-per-share-diluted":
-                    data = requests.request("GET", ll)
-                    url_completo = data.url
-                    hist = scrape_table(url_completo)
-                    hist = hist.sort_index(ascending=False)
-                    print(hist)
-                    #print the name of the columns
-                    #print(hist.columns)
-                    x = hist[company2+ " Annual EPS"]
-                    y = hist[company2 +" Annual EPS.1"]
-                    plt.bar(x, y, label = "Eps")
-                    plt.legend()
-                    plt.title(s)
-                    plt.show()
-                elif ll == "https://www.macrotrends.net/stocks/charts/"+s+"/price-book":
-                    data = requests.request("GET", ll)
-                    url_completo = data.url
-                    hist = scrape_table(url_completo)
-                    hist = hist.sort_index(ascending=False)
-                    hist.dropna()
-                    #print the name of the columns
-                    #print(hist.columns)
-                    x = hist[company2+' Price/Book Ratio Historical Data',                 'Date']
-                    y = hist[company2+' Price/Book Ratio Historical Data',  'Price to Book Ratio']
-                    z = hist[company2+' Price/Book Ratio Historical Data',          'Stock Price']
-                    o = hist[company2+' Price/Book Ratio Historical Data', 'Book Value per Share']
-                    bvpr = []
-                    for gg in o:
-                        if gg == str(gg):
-                            gg = gg[1:]
-                            gg = float(gg)
-                            bvpr.append(gg)
-                        else:
-                            gg = 0
-                            bvpr.append(gg)
-                    hist["bvpr"] = bvpr
-                    zz = hist["bvpr"]
-                    plt.plot(x, y, label = "p/b")
-                    plt.plot(x, z, label="stock price")
-                    plt.legend()
-                    plt.title(s)
-                    plt.show()
-                    plt.bar(x, zz, label="BVPS")
-                    plt.title(s)
-                    plt.legend()
-                    plt.show()
-        else:
-            for i in stocks:
-                if i == company:
-                    #ychart
-                    url = "https://ycharts.com/companies/"+ i +"/profit_margin"
-                    data = requests.request("GET", url)
-                    url_completo = data.url
-                    hist = scrape_table(url_completo)
-                    # sorting the date from the beginning tell the end
-                    hist = hist.sort_index(ascending=False)
-                    print(hist)
-                    #plotting the results
-                    plt.bar(hist["Data for this Date Range"],hist["Unnamed: 1"])
-                    plt.title("Profit margin "+i)
-                    plt.show()
+    if company in stock_list:
+        for s in stock_list:
+            if s == company:
+                #macro
+                url = ["https://www.macrotrends.net/stocks/charts/" + s + "/pe-ratio","https://www.macrotrends.net/stocks/charts/"+ s +"/eps-earnings-per-share-diluted","https://www.macrotrends.net/stocks/charts/"+s+"/price-book" ]
+                for ll in url:
+                    if ll == "https://www.macrotrends.net/stocks/charts/" + s + "/pe-ratio":
+                        data = requests.request("GET",ll)
+                        url_completo = data.url
+                        hist = scrape_table(url_completo)
+                        hist = hist.sort_index(ascending=False)
+                        print(hist)
+                        # print the name of the columns
+                        #print(hist.columns)
+                        x =hist[company2 + ' PE Ratio Historical Data','Date']
+                        y = hist[company2 +' PE Ratio Historical Data', 'PE Ratio']
+                        z= hist[company2 +' PE Ratio Historical Data', 'Stock Price']
+                        plt.plot(x,y, label = "P/E ratio")
+                        plt.plot(x,z, label = "stock price")
+                        plt.legend()
+                        plt.title(s)
+                        plt.show()
+                    elif ll == "https://www.macrotrends.net/stocks/charts/"+ s +"/eps-earnings-per-share-diluted":
+                        data = requests.request("GET", ll)
+                        url_completo = data.url
+                        hist = scrape_table(url_completo)
+                        hist = hist.sort_index(ascending=False)
+                        print(hist)
+                        #print the name of the columns
+                        #print(hist.columns)
+                        x = hist[company2+ " Annual EPS"]
+                        y = hist[company2 +" Annual EPS.1"]
+                        plt.bar(x, y, label = "Eps")
+                        plt.legend()
+                        plt.title(s)
+                        plt.show()
+                    elif ll == "https://www.macrotrends.net/stocks/charts/"+s+"/price-book":
+                        data = requests.request("GET", ll)
+                        url_completo = data.url
+                        hist = scrape_table(url_completo)
+                        hist = hist.sort_index(ascending=False)
+                        hist.dropna()
+                        #print the name of the columns
+                        #print(hist.columns)
+                        x = hist[company2+' Price/Book Ratio Historical Data',                 'Date']
+                        y = hist[company2+' Price/Book Ratio Historical Data',  'Price to Book Ratio']
+                        z = hist[company2+' Price/Book Ratio Historical Data',          'Stock Price']
+                        o = hist[company2+' Price/Book Ratio Historical Data', 'Book Value per Share']
+                        bvpr = []
+                        for gg in o:
+                            if gg == str(gg):
+                                gg = gg[1:]
+                                gg = float(gg)
+                                bvpr.append(gg)
+                            else:
+                                gg = 0
+                                bvpr.append(gg)
+                        hist["bvpr"] = bvpr
+                        zz = hist["bvpr"]
+                        plt.plot(x, y, label = "p/b")
+                        plt.plot(x, z, label="stock price")
+                        plt.legend()
+                        plt.title(s)
+                        plt.show()
+                        plt.bar(x, zz, label="BVPS")
+                        plt.title(s)
+                        plt.legend()
+                        plt.show()
+    else:
+        for i in stocks:
+            if i == company:
+                #ychart
+                url = "https://ycharts.com/companies/"+ i +"/profit_margin"
+                data = requests.request("GET", url)
+                url_completo = data.url
+                hist = scrape_table(url_completo)
+                # sorting the date from the beginning tell the end
+                hist = hist.sort_index(ascending=False)
+                print(hist)
+                #plotting the results
+                plt.bar(hist["Data for this Date Range"],hist["Unnamed: 1"])
+                plt.title("Profit margin "+i)
+                plt.show()
             break
 
 
 #merging the two list to get the info for the all companies
-stock_list_temp = stock_list
 
-merge = stock_list_temp+ stocks
+merge = stock_list + stocks
 
 #printing all results 
 for i in merge:
-    if i in stock_list_temp:
-        for n in stock_list_temp:
+    if i in stock_list:
             for k in names:
-                print(get_url(n,k))
+                print(get_url(i,k))
                 names.remove(k)
-                stock_list_temp.remove(n)
                 break
-            break
     else:
         print(get_url(i,None))
 
